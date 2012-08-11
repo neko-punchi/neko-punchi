ba01c115259067e213e299a373c6ca15c11bd747
shoji <shoji@shoji-VirtualBox.(none)>
2012-08-11 10:15:40 +0900

    initial comment dayo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElearnNekoPunchi::Application.config.secret_token = 'e3dfffe2321dda3a7ab160ba93cdeed54f514f68ef39cfd881174fddd6e69efd792f0d2370129cdd67e2ac29158a46ba4686606f9e719c22ef7f02ee189dc497'
