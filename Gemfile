5db1754001d1cc90389648b4ba204b0096f4ab68
nksm-npe <notpoorelephant@gmail.com>
2012-10-03 22:25:35 +0900

    add google authentication

--- Gemfile
+++ Gemfile
@@ -32,7 +32,7 @@ gem 'jquery-rails'
 gem 'omniauth'
 gem 'omniauth-twitter'
 gem 'omniauth-facebook'
-
+gem 'omniauth-google-oauth2'
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
    provider :facebook,"526309830715742","6c296324d8544a4fbc2114f0a75d31fe"
    provider :twitter,"ZrIZdY0YZNHUiuDhCRWgVA","7PsVO6530NeQ3WUNxp0oUTC0vf9GcojYxEuzc76Jc"
-
+   # Note: access_type can be 'online' (for just authenticating) or 'offline' (for using services)
+   # approval_prompt should be a blank string or else it defaults to 'force', which requires re-authenticating on each login/usage
+   provider :google_oauth2, "757075733479.apps.googleusercontent.com", "IYtsUP2xrU85GXkdhVod6c7M", {:access_type => 'online', :approval_prompt => ''}
 end
