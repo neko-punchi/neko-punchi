ef91063b23c8f8e3ca35a1ef352397b66a3a1a12
haratama <miggy.ss@gmail.com>
2012-11-17 00:46:47 +0900

    change quiz_list

--- app/controllers/quizzes_list_controller.rb
+++ app/controllers/quizzes_list_controller.rb
@@ -1,15 +1,16 @@
 class QuizzesListController < ApplicationController
   def index
-    @objlist = Relation.joins(:quiz, :category, :sub_category)
-    @category_list = Category.all
+    @objlist = Quiz.all
+    #@category_list = Category.all
   end
   
-  def mod
-    #@quiz_id = params[:quiz_id]
-    #@category_id = params[:category]
-    @relation = Relation.find(params[:quiz_id].to_i)
-    @relation.update_attributes(:category_id => params[:category_id])
-    redirect_to  :action  =>  "index"
+  def edit
+    @quiz_id = params[:quiz_id]
+    #@category_id = params[:category]a
+    @quiz_data = Relation.find_by_sql(["select DD.category,BB.sub_category, CC.* from relations AA inner join sub_categories BB on AA.sub_category_id = BB.id inner join quizzes CC on AA.quiz_id = CC.id inner join categories DD on AA.category_id = DD.id where CC.id = :quiz_id", :quiz_id => @quiz_id])
+    #@relation = Relation.find(params[:quiz_id].to_i)
+    #@relation.update_attributes(:category_id => params[:category_id])
+   # redirect_to  :action  =>  "edit"
   
   end
 end
