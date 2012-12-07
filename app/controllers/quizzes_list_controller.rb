41518d34fac1126da767ef0832c294bf39f14c6c
haratama <miggy.ss@gmail.com>
2012-12-08 04:09:59 +0900

    change quiz_list

--- app/controllers/quizzes_list_controller.rb
+++ app/controllers/quizzes_list_controller.rb
@@ -9,31 +9,37 @@ class QuizzesListController < ApplicationController
     #@category_id = params[:category]a
     @quiz_data = Relation.find_by_sql(["select DD.category,BB.sub_category, CC.* from relations AA inner join sub_categories BB on AA.sub_category_id = BB.id inner join quizzes CC on AA.quiz_id = CC.id inner join categories DD on AA.category_id = DD.id where CC.id = :quiz_id", :quiz_id => quiz_id])
     #@relation = Relation.find(params[:quiz_id].to_i)
+    @quiz = Quiz.find(params[:quiz_id])
     #@relation.update_attributes(:category_id => params[:category_id])
    # redirect_to  :action  =>  "edit"
   end
   
-  def update
+  def update_table
     quiz_id = params[:quiz_id]
     #quizzesテーブルのデータをparamの値に更新
     quiz = Quiz.find_by_id(quiz_id)
-    quiz.text = params[:text].strip
-    quiz.sub_text = params[:sub_text].strip
-    quiz.choose = params[:choose].strip
-    quiz.answer = params[:answer].strip
-    quiz.hint = params[:hint].strip
-    quiz.save
+    Quiz.transaction do
+      quiz.text = params[:text].strip
+      quiz.sub_text = params[:sub_text].strip
+      quiz.choose = params[:choose].strip
+      quiz.answer = params[:answer].strip
+      quiz.hint = params[:hint].strip
+      quiz.save
    
-    #relationテーブルの変更
-    relation = Relation.find_by_quiz_id(quiz_id)
-    #findの結果、レコードが存在すれば更新し、0件(nil)の場合は新規に追加する。
-    if relation then
-      relation.category_id = params[:category].to_i
-      relation.sub_category_id = params[:sub_category]
-    else
-      relation = Relation.new(:quiz_id => quiz_id, :category_id => params[:category], :sub_category_id => params[:sub_category])
+      #relationテーブルの変更
+      relation = Relation.find_by_quiz_id(quiz_id)
+    # findの結果、レコードが存在すれば更新し、0件(nil)の場合は新規に追加する。
+      if relation then
+        relation.category_id = params[:category].to_i
+        relation.sub_category_id = params[:sub_category]
+      else
+        relation = Relation.new(:quiz_id => quiz_id, :category_id => params[:category], :sub_category_id => params[:sub_category])
+      end
+      relation.save
     end
-    relation.save
-    redirect_to :controller => "quizzes_list", :action => "edit", :quiz_id => quiz_id
+      #成功時の処理
+      redirect_to :controller => "quizzes_list", :action => "edit", :quiz_id => quiz_id 
+    #resque => e
+      #TODO 失敗時の処理
   end
 end
