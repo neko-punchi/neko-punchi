58711166f0f72450515f1cc3a44b23a458d4233f
shoji <shoji@shoji-VirtualBox.(none)>
2012-12-15 13:33:11 +0900

    modified your_records visual image

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -2,17 +2,17 @@ document.addEventListener ('DOMContentLoaded', function(){
 	var rate_elems = $('td.rate');
 	for(var i = 0; i < rate_elems.length; i++){
 		if        ( rate_elems[i].textContent == 100 ) {
-			rate_elems[i].bgColor = "99ff00" ;
+			rate_elems[i].bgColor = "#99ffff" ;
 		} else if ( rate_elems[i].textContent > 90 ) {
-			rate_elems[i].bgColor = "#ccff33" ;
+			rate_elems[i].bgColor = "#ccff66" ;
 		} else if ( rate_elems[i].textContent > 80 ) {
-			rate_elems[i].bgColor = "#ccff99" ;
+			rate_elems[i].bgColor = "#ccff33" ;
 		} else if ( rate_elems[i].textContent > 60 ) {
-			rate_elems[i].bgColor = "#ffcc00" ;
+			rate_elems[i].bgColor = "#ffcccc" ;
 		} else if ( rate_elems[i].textContent > 40 ) {
-			rate_elems[i].bgColor = "#ff6600" ;
+			rate_elems[i].bgColor = "#ff9999" ;
 		} else if ( rate_elems[i].textContent > 20 ) {
-			rate_elems[i].bgColor = "#ff3300" ;
+			rate_elems[i].bgColor = "#ff3366" ;
 		} else if ( rate_elems[i].textContent > 0 ) {
 			rate_elems[i].bgColor = "#ff3300" ;
 		} else {
