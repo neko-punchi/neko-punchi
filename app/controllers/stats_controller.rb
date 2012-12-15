05165f34c49b4e52d9d20679c90e58c5a7f00d2b
nksm-npe <notpoorelephant@gmail.com>
2012-12-15 11:39:57 +0900

    modify your_records

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,45 +2,45 @@ class StatsController < ApplicationController
   require "pp"
   def your_records
   	
-    @category = Category.find(:all,:order => "id ASC")
-    @sub_category = SubCategory.find(:all,:order=>"id ASC")
-    pp @category
-    pp @sub_category
+    categories = Category.find(:all,:order => "id ASC")
+    sub_categories = SubCategory.find(:all,:order=>"id ASC")
+    #pp categories
+    #pp sub_categories
 
     @your_answer_rates = []
 
-    #
+    #ループ内で使用する作業領域の初期化
     buff = nil
     correct_sub = 0
     wrong_sub = 0
+    ans_rate_buff = nil
 
-    @sub_category.each do |sc|
-      buff = PersonalResult.find_all_by_sub_category_id(sc.id)
-      if  buff == [] then
-        @your_answer_rates<<{
-          :category => @category[sc.category_id - 1][:category],
-          :sub_category => @sub_category[sc.id - 1][:sub_category],
-          :correct_sub => 0,
-          :try_total => 0,
-          :rate => 0 
-        }
+    sub_categories.each do |sc|
+      #p '#############'
+      #pp buff
+      ans_rate_buff = Hash.new
+      ans_rate_buff[:category] = get_category_name(categories,sc.category_id)
+      ans_rate_buff[:sub_category] = get_sub_category_name(sub_categories,sc.id)
+
+      buff = PersonalResult.find_by_sub_category_id(sc.id)
+      
+      if  buff == nil then
+        ans_rate_buff[:correct_sub] = 0
+        ans_rate_buff[:try_total] = 0
+        ans_rate_buff[:rate] = 0 
       else
-        buff.each do |pr|
-          correct_sub += pr.correct
-          wrong_sub += pr.wrong
-        end
-        @your_answer_rates<<{
-          :category => @category[sc.category_id - 1][:category],
-          :sub_category => @sub_category[sc.id - 1][:sub_category],
-          :correct_sub => correct_sub,
-          :try_total => correct_sub + wrong_sub,
-          :rate => (correct_sub*100)/(correct_sub + wrong_sub) 
-        }
+        ans_rate_buff[:correct_sub] = buff[:correct]
+        ans_rate_buff[:try_total] = buff[:correct] + buff[:wrong]
+        ans_rate_buff[:rate] = ( buff[:correct] * 100 )/( buff[:correct] + buff[:wrong] ) 
       end
 
-    buff = nil
-    correct_sub = 0
-    wrong_sub = 0
+      @your_answer_rates<<ans_rate_buff  
+    
+      #ループ内で使用する作業領域の初期化
+      buff = nil
+      correct_sub = 0
+      wrong_sub = 0
+      ans_rate_buff = nil
 
     end
    
@@ -50,6 +50,34 @@ class StatsController < ApplicationController
   		format.html
   	end
   end
+
+
+  private
+  def get_category_name(category_db,category_id)
+    category_db.each do |line|
+      if line[:id] == category_id then
+        return line[:category]
+      end
+    end
+
+    # if not found any name ...
+    p "warning! category_name is nil."
+    return nil
+  end
+
+  def get_sub_category_name(sub_category_db,category_id)
+    sub_category_db.each do |line|
+      if line[:id] == category_id then
+        return line[:sub_category]
+      end
+    end
+    # if not found any name ...
+    p "warning! sub_category_name is nil."
+    return nil
+  end
+
+
+  public
 
   def statistics
   end
